16c1984e2c4adcc4db0a097c9582d1a0961562f7
Alex Krantz <alex@alexkrantz.com>
2019-09-06 13:25:40 -0700

    Add logging config validation and setting

--- api/init.go
+++ api/init.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"log"
 	"os"
@@ -16,6 +17,8 @@ func init() {
 	viper.SetDefault("http.port", 8080)
 	viper.SetDefault("http.domain", "http://127.0.0.1:8080")
 	viper.SetDefault("http.reset_files", false)
+	viper.SetDefault("logging.format", "json")
+	viper.SetDefault("logging.level", "info")
 	viper.SetDefault("database.host", "127.0.0.1")
 	viper.SetDefault("database.port", 5432)
 	viper.SetDefault("database.username", "postgres")
@@ -51,5 +54,37 @@ func init() {
 		if err := os.Mkdir("./uploaded", os.ModePerm); err != nil {
 			log.Fatalf("Failed to create uploads directory: %v", err)
 		}
+	}
+
+	// Validate and set log format
+	if format := viper.GetString("logging.format"); format != "text" && format != "json" {
+		log.Fatalf("Invalid format '%s' for log output format, must be one of: text, json", format)
+	} else if format == "json" {
+		logrus.SetFormatter(&logrus.JSONFormatter{})
+	} else if format == "text" {
+		logrus.SetFormatter(&logrus.TextFormatter{
+			FullTimestamp: true,
+			DisableLevelTruncation: true,
+		})
+	}
+
+	// Validate and set log level
+	switch viper.GetString("logging.level") {
+	case "trace":
+		logrus.SetLevel(logrus.TraceLevel)
+	case "debug":
+		logrus.SetLevel(logrus.DebugLevel)
+	case "info":
+		logrus.SetLevel(logrus.InfoLevel)
+	case "warn":
+		logrus.SetLevel(logrus.WarnLevel)
+	case "error":
+		logrus.SetLevel(logrus.ErrorLevel)
+	case "fatal":
+		logrus.SetLevel(logrus.FatalLevel)
+	case "panic":
+		logrus.SetLevel(logrus.PanicLevel)
+	default:
+		log.Fatalf("Invalid level '%s' for minimum log level, must be one of: trace, debug, info, warn, error, fatal, panic", viper.GetString("logging.level"))
 	}
 }
