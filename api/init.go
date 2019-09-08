52998736dee6f058ef31db7cc540c30e74cd341e
Alex Krantz <alex@alexkrantz.com>
2019-09-07 18:24:45 -0700

    Add logging to initialization function

--- api/init.go
+++ api/init.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
-	"log"
 	"os"
 )
 
@@ -15,10 +14,12 @@ func init() {
 		FullTimestamp:          true,
 		DisableLevelTruncation: true,
 	})
+	logrus.WithField("app", "initialization").Trace("Configured base logger settings")
 
 	// Setup configuration file
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
+	logrus.WithField("app", "initialization").Trace("Set configuration file path and name")
 
 	// Initialize configuration default
 	viper.SetDefault("http.host", "127.0.0.1")
@@ -34,6 +35,7 @@ func init() {
 	viper.SetDefault("database.database", "postgres")
 	viper.SetDefault("database.ssl", "disable")
 	viper.SetDefault("database.reset", false)
+	logrus.WithField("app", "initialization").Trace("Set defaults for configuration keys")
 
 	// Parse configuration file
 	if err := viper.ReadInConfig(); err != nil {
@@ -44,40 +46,47 @@ func init() {
 			logrus.WithField("app", "initialization").WithError(err).Fatal("Failed to parse configuration file")
 		}
 	}
+	logrus.WithField("app", "initialization").Trace("Successfully parsed configuration file")
 
 	// Validate ssl mode
 	if mode := viper.GetString("database.ssl"); mode != "disable" && mode != "allow" && mode != "prefer" && mode != "require" && mode != "verify-ca" && mode != "verify-full" {
 		logrus.WithFields(logrus.Fields{"app": "initialization", "key": "database.ssl", "value": viper.GetString("database.ssl"), "options": []string{"disable", "allow", "prefer", "require", "verify-ca", "verify-full"}}).Fatal("Invalid value for database ssl mode")
 	}
+	logrus.WithField("app", "initialization").Trace("Validated database ssl connection mode")
 
 	// Delete all uploaded files
 	if viper.GetBool("http.reset_files") {
 		if err := os.RemoveAll("./uploaded"); err != nil {
 			logrus.WithError(err).WithFields(logrus.Fields{"app": "initialization", "key": "http.reset_files"}).Fatal("Failed to reset uploaded files")
 		}
+		logrus.WithField("app", "initialization").Info("Successfully deleted all uploaded files")
 	}
+	logrus.WithField("app", "initialization").Trace("Optionally deleted all uploaded files")
 
 	// Create directory if not exist
 	if _, err := os.Stat("./uploaded"); os.IsNotExist(err) {
 		if err := os.Mkdir("./uploaded", os.ModePerm); err != nil {
-			log.Printf("Failed to create uploads directory: %v", err)
 			logrus.WithField("app", "initialization").WithError(err).Fatal("Failed to create uploads directory")
 		}
+		logrus.WithField("app", "initialization").Debug("Created uploads directory")
 	} else if err != nil {
 		logrus.WithField("app", "initialization").WithError(err).Fatal("Failed to stat uploads directory")
 	}
+	logrus.WithField("app", "initialization").Trace("Created uploads directory if it did not exist")
 
 	// Validate and set log format
 	if format := viper.GetString("logging.format"); format != "text" && format != "json" {
 		logrus.WithFields(logrus.Fields{"app": "initialization", "key": "logging.format", "value": format, "options": []string{"text", "json"}}).Fatal("Invalid output format specified")
 	} else if format == "json" {
 		logrus.SetFormatter(&logrus.JSONFormatter{})
+		logrus.WithField("app", "initialization").Debug("Set output format to JSON")
 	} else if format == "text" {
 		logrus.SetFormatter(&logrus.TextFormatter{
 			DisableTimestamp:       false,
 			FullTimestamp:          true,
 			DisableLevelTruncation: true,
 		})
+		logrus.WithField("app", "initialization").Debug("Set output format to text")
 	}
 
 	// Validate and set log level
@@ -99,4 +108,5 @@ func init() {
 	default:
 		logrus.WithFields(logrus.Fields{"app": "initialization", "key": "logging.level", "value": viper.GetString("logging.level"), "options": []string{"trace", "debug", "info", "warn", "error", "fatal", "panic"}}).Fatal("Invalid level for minimum log level")
 	}
+	logrus.WithField("app", "initialization").Tracef("Set minimum log level to %s", viper.GetString("logging.level"))
 }
