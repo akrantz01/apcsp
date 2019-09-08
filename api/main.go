8d2008a5a33d58ec557d54eb94a3921261cedbf5
Alex Krantz <alex@alexkrantz.com>
2019-09-07 21:37:13 -0700

    Add logging to main

--- api/main.go
+++ api/main.go
@@ -24,42 +24,54 @@ import (
 var shutdown = make(chan os.Signal, 1)
 
 func main() {
+	logger := logrus.WithField("app", "main")
+
 	// Connect to the database
 	db := database.SetupDatabase()
 
 	// Setup routes
 	router := mux.NewRouter()
+	logger.Trace("Created HTTP router")
 
 	// Enable authentication middleware
 	router.Use(authMiddleware(db))
+	logger.Trace("Initialized API authentication middleware")
 
 	// API sub-router
 	api := router.PathPrefix("/api").Subrouter()
+	logger.Trace("Initialized API subrouter")
 
 	// Authentication routes
 	api.HandleFunc("/auth/login", authentication.Login(db)).Methods("POST")
 	api.HandleFunc("/auth/logout", authentication.Logout(db)).Methods("GET")
+	logger.Trace("Add authentication routes")
 
 	// User routes
 	api.HandleFunc("/users", users.AllUsers(db)).Methods("GET", "POST")
 	api.HandleFunc("/users/{user}", users.SpecificUser(db)).Methods("GET", "PUT", "DELETE")
+	logger.Trace("Add user management routes")
 
 	// Chat routes
 	api.HandleFunc("/chats", chats.AllChats(db)).Methods("GET", "POST")
 	api.HandleFunc("/chats/{chat}", chats.SpecificChat(db)).Methods("GET", "DELETE")
+	logger.Trace("Add chat management routes")
 
 	// Messages routes
 	api.HandleFunc("/chats/{chat}/messages", messages.AllMessages(db)).Methods("GET", "POST")
 	api.HandleFunc("/chats/{chat}/messages/{message}", messages.SpecificMessage(db)).Methods("GET", "PUT", "DELETE")
+	logger.Trace("Add chat message management routes")
 
 	// Files routes
 	api.HandleFunc("/files/{file}", files.Files(db)).Methods("GET", "POST")
+	logger.Trace("Add file management routes")
 
 	// Register router with http and enable cors
 	http.Handle("/", handlers.LoggingHandler(os.Stdout, cors.AllowAll().Handler(router)))
+	logger.Trace("Register router with http handler")
 
 	// Wait for OS shutdown signals
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	logger.Trace("Begin listening for SIGINT and SIGTERM")
 
 	// Setup HTTP server
 	server := &http.Server{
@@ -80,15 +92,18 @@ func main() {
 			logrus.WithError(err).WithField("app", "http-server").Fatal("Failure while server was running")
 		}
 	}()
+	logger.Trace("Start server in separate goroutine")
 
 	// Wait for server shutdown
 	<-shutdown
+	logger.Trace("Shutdown signal received. Shutdown sequence started")
 
 	// Create shutdown context
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer func() {
 		cancel()
 	}()
+	logger.Trace("Create shutdown context, forcing after 5 seconds")
 
 	// Shutdown the server gracefully
 	if err := server.Shutdown(ctx); err != nil {
