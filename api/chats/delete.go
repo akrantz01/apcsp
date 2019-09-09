dd52ba8230b1c7f29b5fd38e2844a91d7e8de333
Alex Krantz <alex@alexkrantz.com>
2019-09-08 17:26:01 -0700

    Add logging to chat routes

--- api/chats/delete.go
+++ api/chats/delete.go
@@ -5,38 +5,52 @@ import (
 	"github.com/akrantz01/apcsp/api/util"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func deleteMethod(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
+	logger := logrus.WithFields(logrus.Fields{"app": "chats", "remote_address": r.RemoteAddr, "path": "/api/chats/{chat}", "method": "DELETE"})
+
 	// Validate initial request on path parameters
 	vars := mux.Vars(r)
 	if _, ok := vars["chat"]; !ok {
+		logger.WithField("chat", vars["chat"]).Trace("Invalid value for path parameter")
 		util.Responses.Error(w, http.StatusBadRequest, "path parameter 'chat' must be present")
 		return
 	}
+	logger.WithField("chat", vars["chat"]).Trace("Validated initial request on path parameters")
+
+	// Add chat id to logger
+	logger = logger.WithField("chat", vars["chat"])
 
 	// Ensure chat exists
 	var chat database.Chat
 	db.Preload("Users").Where("uuid = ?", vars["chat"]).First(&chat)
 	if chat.ID == 0 {
+		logger.Trace("Specified chat does not exist")
 		util.Responses.Error(w, http.StatusBadRequest, "specified chat does not exist")
 		return
 	}
+	logger.Trace("Retrieved chat information from database")
 
 	// Get token w/o validation
 	token, err := util.JWT.Unvalidated(r.Header.Get("Authorization"))
 	if err != nil {
+		logger.WithError(err).Error("Unable to get unvalidated token")
 		util.Responses.Error(w, http.StatusInternalServerError, "failed to get token parts")
 		return
 	}
+	logger.Trace("Got unvalidated token parts")
 
 	// Get user id from token
 	id, err := util.JWT.UserId(token)
 	if err != nil {
+		logger.WithError(err).Trace("Failed to get user id from token")
 		util.Responses.Error(w, http.StatusBadRequest, err.Error())
 		return
 	}
+	logger.WithField("uid", id).Trace("Got user id from token")
 
 	// Get user from database
 	var user database.User
@@ -45,6 +59,7 @@ func deleteMethod(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 		util.Responses.Error(w, http.StatusBadRequest, "specified user does not exist")
 		return
 	}
+	logger.WithField("uid", id).Trace("Retrieved user data from database")
 
 	// Ensure user is in chat
 	valid := false
@@ -55,13 +70,16 @@ func deleteMethod(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 		}
 	}
 	if !valid {
+		logger.WithField("uid", id).Trace("User associated with token not in chat")
 		util.Responses.Error(w, http.StatusForbidden, "specified user is not in chat")
 		return
 	}
+	logger.WithField("uid", id).Trace("Confirmed requesting user in chat")
 
 	// Delete chat and messages
 	db.Delete(database.Message{}, "chat_id = ?", chat.ID)
 	db.Delete(&chat)
 
 	util.Responses.Success(w)
+	logger.Debug("Deleted chat and all messages")
 }
--- api/chats/list.go
+++ api/chats/list.go
@@ -4,24 +4,31 @@ import (
 	"github.com/akrantz01/apcsp/api/database"
 	"github.com/akrantz01/apcsp/api/util"
 	"github.com/jinzhu/gorm"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func list(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
+	logger := logrus.WithFields(logrus.Fields{"app": "chats", "remote_address": r.RemoteAddr, "path": "/api/users", "method": "GET"})
+
 	// Get token w/o validation
 	token, err := util.JWT.Unvalidated(r.Header.Get("Authorization"))
 	if err != nil {
+		logger.WithError(err).Error("Unable to get unvalidated token")
 		util.Responses.Error(w, http.StatusInternalServerError, "failed to get token parts")
 		return
 	}
+	logger.Trace("Got unvalidated token parts")
 
 	// Retrieve current user
 	var user database.User
 	db.Preload("Chats").Preload("Chats.Users").Preload("Chats.Messages").Preload("Chats.Messages.Sender").Preload("Chats.Messages.File").Where("id = ?", util.JWT.Claims(token)["sub"]).First(&user)
+	logger.Trace("Got current user's chats and messages")
 
 	// Return empty array if no data
 	if len(user.Chats) == 0 {
 		util.Responses.SuccessWithData(w, []string{})
+		logger.WithFields(logrus.Fields{"chats": len(user.Chats), "id": user.ID}).Debug("Got list of chats for user")
 		return
 	}
 
@@ -29,6 +36,8 @@ func list(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
 	for index, chat := range user.Chats {
 		user.Chats[index].Messages = []database.Message{chat.Messages[len(chat.Messages)-1]}
 	}
+	logger.Trace("Set most recent message to only message in chat")
 
 	util.Responses.SuccessWithData(w, user.Chats)
+	logger.WithFields(logrus.Fields{"chats": len(user.Chats), "id": user.ID}).Debug("Got list of chats for user")
 }
